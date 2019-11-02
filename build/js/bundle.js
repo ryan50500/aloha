81ff2f29a1df870675f046d60333ec2f249f77e9
Ryan Woods <ryanwoods@ryans-air.home>
2019-11-02 15:07:54 +0000

    aloha files

---
+++ build/js/bundle.js
@@ -0,0 +1,56 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+
+
+$('.main-carousel').flickity({
+  cellAlign: 'left',
+  contain: true,
+  groupCells: true,
+  wrapAround: true
+
+});
+
+$("input[type='submit']").click(function () {
+
+  var submit = $("input[type ='email']").val();
+
+  if (submit !== "") {
+    alert("Thanks " + submit + " for subscribing!");
+  } else {
+    alert("you need to put email address");
+  }
+});
+
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+
+.not('[href="#0"]').click(function (event) {
+  // On-page links
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function () {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) {
+          // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
+
+},{}]},{},[1]);
